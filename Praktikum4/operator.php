759aca690baec46220df9b12797060e087df6e77
nathanaelGracedo <nathanjuan56@gmail.com>
2024-09-17 09:51:48 +0700

    Operator pada PHP - Bagian 3

---
+++ Praktikum4/operator.php
@@ -0,0 +1,19 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Bilangan A: 10 <br> Bilangan B: 5 <br><br>";
+echo "Hasil Jumlah A dan B: $hasilTambah <br>";
+echo "Hasil Kurang A dan B: $hasilKurang <br>";
+echo "Hasil kali A dan B: $hasilKali <br>";
+echo "Hasil bagi A dan B: $hasilBagi <br>";
+echo "Sisa pembagian A dan B: $sisaBagi <br>";
+echo "Hasil Pemangkatan A dan B: $pangkat";
+?>
