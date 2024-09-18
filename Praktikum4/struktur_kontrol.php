c0707a97e939acb2afbb8f9beef80a54a73e1992
nathanaelGracedo <nathanjuan56@gmail.com>
2024-09-18 22:53:18 +0700

    foreach php - praktikum bagian 4

--- Praktikum4/struktur_kontrol.php
+++ Praktikum4/struktur_kontrol.php
@@ -34,4 +34,12 @@ for ($i = 1; $i <= $jumlahLahan; $i++) {
 
 echo "<br>Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor";
 ?>
