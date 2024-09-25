a6b7ac2156ba1f5c33a53f4486429ed382010a14
nathanaelGracedo <nathanjuan56@gmail.com>
2024-09-25 11:02:58 +0700

    Function PHP - Jobsheet 5

---
+++ Praktikum5/praktik-php/Fungsi.php
@@ -0,0 +1,11 @@
+<?php 
+    function perkenalan() {
+        echo "Assalamualaikum, ";
+        echo "Perkennalkan, nama saya Elok</br>";
+        echo "Senang berkenalan dengan anda </br>";
+    }
+
+    //memanggil fungsi yang sudah dimuat
+    perkenalan();
+    perkenalan();
+?>
