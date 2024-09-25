d7586be32393acf983221a98e7dec35d53449d92
nathanaelGracedo <nathanjuan56@gmail.com>
2024-09-25 11:15:20 +0700

    Parameter dengan Nilai Default PHP - Jobsheet 5

--- Praktikum5/praktik-php/Fungsi.php
+++ Praktikum5/praktik-php/Fungsi.php
@@ -1,5 +1,5 @@
 <?php 
-    function perkenalan($nama, $salam) {
+    function perkenalan($nama, $salam="Assalamualaikum") {
         echo $salam. ", ";
         echo "Perkennalkan, nama saya ".$nama."</br>";
         echo "Senang berkenalan dengan anda </br>";
@@ -13,6 +13,6 @@
     $saya = "Nathan";
     $ucapanSalam = "Selamat pagi";
 
-    //memanggil lagi
-    perkenalan($saya, $ucapanSalam);
+    //memanggil lagi tanpa mengisi parameter salam
+    perkenalan($saya);
 ?>
