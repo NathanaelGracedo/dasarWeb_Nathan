b47ea8fc0a64a9550104df29f35e7367cac4cf2b
nathanaelGracedo <nathanjuan56@gmail.com>
2024-09-25 11:29:24 +0700

    Memanggil fungsi dalam fungsi PHP - Jobsheet 5

--- Praktikum5/praktik-php/FungsiReturn.php
+++ Praktikum5/praktik-php/FungsiReturn.php
@@ -3,6 +3,15 @@
         $umur = $thn_sekarang - $thn_lahir;
         return $umur;
     }    
+    function perkenalan($nama, $salam="Assalamualaikum") {
+        echo $salam.", ";
+        echo "Perkenalkan, nama saya ".$nama."</br>";
 
-    echo "Umur saya adalah ". hitungUmur(2004, 2024) ." tahun"
+        //memanggil fungsi lain
+        echo "Saya berusia ". hitungUmur(2004, 2024)." tahun<br/>";
+        echo "Senang berkenalan dengan anda<br/>";
+    }
+
+    //memanggil fungsi perkenalan
+    perkenalan("Nathan");
 ?>
