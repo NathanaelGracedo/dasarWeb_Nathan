9cdef4d1a2ae04cb327a935dee363501838038d8
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-01 07:46:33 +0700

    Array2 Output Tabel PHP - Jobsheet 6

--- Praktikum6/Array2.php
+++ Praktikum6/Array2.php
@@ -12,10 +12,20 @@
                     'domisili' => 'Malang',
                     'jenis_kelamin' => 'Perempuan' 
                 ];
-
-                echo "Nama : {$Dosen ['nama']} <br>";
-                echo "Domisili : {$Dosen ['domisili']} <br>";
-                echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
             ?>
+            
+            <h2>Biodata Dosen</h2>
+            <table border="1" cellpadding="10" cellspacing="0">
+                <tr>
+                    <th style="background-color: #B7E0FF">Nama</th>
+                    <th style="background-color: #B7E0FF">Domisili</th>
+                    <th style="background-color: #B7E0FF">Jenis Kelamin</th>
+                </tr>
+                <tr>
+                    <td><?php echo $Dosen['nama']; ?></td>
+                    <td><?php echo $Dosen['domisili']; ?></td>
+                    <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+                </tr>
+            </table>
         </body>
     </html>
