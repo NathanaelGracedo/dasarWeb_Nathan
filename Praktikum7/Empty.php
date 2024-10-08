e13884f2e6edbe701616435b337482a0ebcd3f7f
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-08 07:49:47 +0700

    Function Empty - Jobsheet 7 Soal 2.2

--- Praktikum7/Empty.php
+++ Praktikum7/Empty.php
@@ -2,8 +2,15 @@
     // Soal 2.1
     $myArray = array();
     if (empty($myArray)) {
-        echo "Array tidak terdefinisi atau kosong.";
+        echo "Array tidak terdefinisi atau kosong.<br>";
     } else {
         echo "Array terdefinisi dan tidak kosong";
+    }
+
+    // Soal 2.2
+    if (empty($nonExistentVar)) {
+        echo "Variabel tidak terdefinisi atau kosong.";
+    } else {
+        echo "Variabel terdefinisi dan tidak kosong.";
     }
 ?>
