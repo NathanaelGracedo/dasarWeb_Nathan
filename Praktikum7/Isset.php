f9e2a439c1372ca5a808f8eae8d351b932d3914c
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-08 07:39:42 +0700

    Function Isset - Jobsheet 7 Soal 1.2

--- Praktikum7/Isset.php
+++ Praktikum7/Isset.php
@@ -1,8 +1,17 @@
 <?php 
-    $umur;
-    if (isset($umur) && $umur >= 18) {
-        echo "Anda sudah dewasa";
+    // Soal 1.1
+    // $umur;
+    // if (isset($umur) && $umur >= 18) {
+    //     echo "Anda sudah dewasa";
+    // } else {
+    //     echo "Anda bellum dewasa atau variabel 'umur' tidak ditemukan";
+    // }
+
+    // Soal 1.2
+    $data = array("nama" => "Juan", "usia" => 25);
+    if (isset($data["nama"])) {
+        echo "Nama: <br>" . $data["nama"];
     } else {
-        echo "Anda bellum dewasa atau variabel 'umur' tidak ditemukan";
+        echo "Variabel 'nama' tidak ditemukan dalam array";
     }
 ?>
