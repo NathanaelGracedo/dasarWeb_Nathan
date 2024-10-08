431a15cdc125fdfaac20e9927eba4d26a3737466
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-08 08:39:29 +0700

    HTML Injection Email - Jobsheet 7 Soal 4.2

--- Praktikum7/html_aman.php
+++ Praktikum7/html_aman.php
@@ -8,15 +8,28 @@
         <form method="post" action="html_aman.php">
             <label for="input">Nama:</label>
             <input type="text" name="input" id="input" required><br><br>
+            
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required><br><br>
+
             <input type="submit" value="Submit">
         </form>
-
+        
         <?php 
+        // Soal 4.1 
             if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 $input = $_POST['input'];
                 $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
                 
-                echo "Nama: " . $input;
+                echo "Nama: " . $input . "<br>";
+                
+        // Soal 4.2
+                $email = $_POST['email'];
+                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    echo "Email valid: " . htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+                } else {
+                    echo "Email tidak valid!";
+                }
             }
         ?>
     </body>
