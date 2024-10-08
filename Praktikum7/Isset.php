3376d52dfba3c604117e98b840fb6e8c87784a01
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-08 07:31:57 +0700

    Function Isset - Jobsheet 7 Soal 1.1

---
+++ Praktikum7/Isset.php
@@ -0,0 +1,8 @@
+<?php 
+    $umur;
+    if (isset($umur) && $umur >= 18) {
+        echo "Anda sudah dewasa";
+    } else {
+        echo "Anda bellum dewasa atau variabel 'umur' tidak ditemukan";
+    }
+?>
