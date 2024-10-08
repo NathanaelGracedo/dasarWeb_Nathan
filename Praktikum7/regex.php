a430088e0ba7e8491eaa00b0cb1f58a892afaa3e
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-08 09:47:05 +0700

    Regex PHP - Jobsheet 7 Soal 5.2

--- Praktikum7/regex.php
+++ Praktikum7/regex.php
@@ -1,9 +1,19 @@
 <?php 
+    // Soal 5.1
     $pattern = '/[a-z]/';
     $text = 'This is a Sample Text.';
     if (preg_match($pattern, $text)) {
-        echo "Huruf kecil ditemukan!";
+        echo "Huruf kecil ditemukan! <br>";
     } else {
         echo "Tidak ada huruf kecil!";
+    }
+
+    // Soal 5.2
+    $pattern = '/[0-9]+/';
+    $text = 'There are 123 apples.';
+    if (preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: ". $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
     }
 ?>
