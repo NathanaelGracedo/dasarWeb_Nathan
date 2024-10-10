71223f0af4b50026f0cf5b8a026663a0e7063f15
nathanaelGracedo <nathanjuan56@gmail.com>
2024-10-10 16:27:51 +0700

    Form Validasi dengan AJAX - Jobsheet 7 Soal 7.3

--- Praktikum7/form_validasi.php
+++ Praktikum7/form_validasi.php
@@ -1,50 +1,73 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Form Input dengan Validasi</title>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    </head>
-    <body>
-        <h1>Form Input dengan Validasi</h1>
-        <form id="myForm" method="post" action="proses_validasi.php">
-            <label for="nama">Nama:</label>
-            <input type="text" name="nama" id="nama">
-            <span id="nama-eror" style="color: red;"></span>
-            <br>
+<head>
+    <title>Form Input dengan Validasi dan AJAX</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+<body>
+    <h1>Form Input dengan Validasi dan AJAX</h1>
+    <form id="myForm">
+        <label for="nama">Nama:</label>
+        <input type="text" id="nama" name="nama">
+        <span id="nama-error" style="color: red;"></span><br>
 
-            <label for="email">Email:</label>
-            <input type="text" name="email" id="email">
-            <span id="email-eror" style="color: red;"></span>
-            <br>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+        <span id="email-error" style="color: red;"></span><br>
 
-            <input type="submit" value="Submit">
-        </form>
+        <input type="submit" value="Submit">
+    </form>
 
-        <script>
-            $(document).ready(function() {
-                $("#myForm").submit(function(event) {
-                    var nama = $("#nama").val();
-                    var email = $("#email").val();
-                    var valid = true;
+    <div id="response"></div>
 
-                    if (nama === "") {
-                        $("#nama-error").text("Nama harus diisi.");
-                        valid = false;
-                    } else {
-                        $("#nama-error").text("");
-                    }
+    <script>
+        $(document).ready(function () {
+            $("#myForm").submit(function (event) {
+                event.preventDefault();
 
-                    if (email === "") {
-                        $("#email-error").text("Email harrus diisi.");
-                    } else {
-                        $("#email-error").text("");
-                    }
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
 
-                    if (valid) {
-                        !event.preventDefault();
-                    }
-                });
+                $("#nama-error").text("");
+                $("#email-error").text("");
+
+                if (nama === "") {
+                    $("#nama-error").text("Nama harus diisi.");
+                    valid = false;
+                }
+
+                if (email === "") {
+                    $("#email-error").text("Email harus diisi.");
+                    valid = false;
+                } else if (!validateEmail(email)) {
+                    $("#email-error").text("Format email tidak valid.");
+                    valid = false;
+                }
+
+                if (valid) {
+                    $.ajax({
+                        url: "proses_validasi.php", // Ganti dengan file PHP untuk memproses data
+                        type: "POST",
+                        data: {
+                            nama: nama,
+                            email: email
+                        },
+                        success: function (response) {
+                            $("#response").html(response);
+                        },
+                        error: function () {
+                            $("#response").html("Terjadi kesalahan saat mengirim data.");
+                        }
+                    });
+                }
             });
-        </script>
-    </body>
+
+            function validateEmail(email) {
+                var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return re.test(String(email).toLowerCase());
+            }
+        });
+    </script>
+</body>
 </html>
