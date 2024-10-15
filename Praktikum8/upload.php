88ec0f8e0a19303c37e837242d0141f019ceabc1
NathanaelGracedo <nathanjuan56@gmail.com>
2024-10-15 09:09:28 +0700

    Modify upload.php tampil gambar ketika file berhasil diunggah - Jobsheet 8 Soal 5

--- Praktikum8/upload.php
+++ Praktikum8/upload.php
@@ -7,10 +7,11 @@
         $allowedExtensions = array("jpg", "jpeg", "png", "gif");
         $maxsize = 5*1024*1024;
 
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"]<=$maxsize)
-    {    
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-                echo "File berhasil diunggah";
+        if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {    
+            if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
+                echo "File berhasil diunggah<br>";
+                
+                echo "<img src='".$targetfile."' width='200' alt='thumbnail'>";
             } else {
                 echo "Gagal mengunggah file";
             }
