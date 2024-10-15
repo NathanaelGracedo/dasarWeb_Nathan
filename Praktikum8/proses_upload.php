3c0fc75359c68c6af38aad3ad382eabd86c348b2
NathanaelGracedo <nathanjuan56@gmail.com>
2024-10-15 10:05:29 +0700

    Modify Form Multiupload - Jobsheet 8 Soal 9

--- Praktikum8/proses_upload.php
+++ Praktikum8/proses_upload.php
@@ -1,5 +1,5 @@
 <?php
-    $targetDirectory = "documents/";
+    $targetDirectory = "images/";
 
     if (!file_exists($targetDirectory)) {
         mkdir($targetDirectory, 0777, true);
@@ -11,14 +11,22 @@
         for ($i=0; $i < $totalFiles ; $i++) { 
             $fileName = $_FILES['files']['name'][$i];
             $targetFile = $targetDirectory . $fileName;
+            $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
+            $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+    if (in_array($fileType, $allowedExtensions)) {
             if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
                 echo "File $fileName berhasil diunggah.<br>";
+                echo "<img src='$targetFile' width='200' style='margin: 10px;' alt='$fileName'><br>";
             } else {
                 echo "Gagal mengunggah file $fileName.<br>";
+                }
+            } else {
+                echo "File $fileName memiliki format yang tidak diizinkan.<br>";
             }
         }
     } else {
-        echo "Tidak ada file yang diunggah";
+        echo "Tidak ada file yang diunggah.";
     }
 ?>
